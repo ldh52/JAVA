1768dac38ff79a76da7dbbc844c64d57f5303142
ldh52 <166485055+ldh52@users.noreply.github.com>
2024-08-03 12:34:42 +0900

    Update FileIO.java

--- networkPDS/FileIO.java
+++ networkPDS/FileIO.java
@@ -10,8 +10,7 @@ public class FileIO
     private static String savePath = "C:/test/upfiles/";
     private static String serialPath = "C:/test/data/file_list.ser";
 
-    public byte[] load(String fname) 
-    {
+    public byte[] load(String fname) {
         File f = new File(srcPath + fname);
         if (!f.exists()) {
             System.err.println(f.getPath() + " 파일이 없습니다.");
@@ -30,52 +29,49 @@ public class FileIO
 
     public boolean save(String fname, byte[] fdata) {
         if (fname == null || fname.isEmpty() || fdata == null) {
-            return false;
+		return false;
         }
 
         try {
-        	FileOutputStream fos = new FileOutputStream(savePath + fname);
-            fos.write(fdata);
-            fos.close();
-            return true;
+		FileOutputStream fos = new FileOutputStream(savePath + fname);
+		fos.write(fdata);
+		fos.close();
+		return true;
         } catch (Exception e) {
-            e.printStackTrace();
+		e.printStackTrace();
         }
         return false;
     }
 
     public List<PDSVO> deserialize() {
-    	
-    	File f = new File(serialPath);
-    	if(!f.exists()) {
+	    File f = new File(serialPath);
+	    if(!f.exists()) {
     		return new ArrayList<PDSVO>();
-    	}
-    	try {
-			ObjectInputStream oin = new ObjectInputStream(new FileInputStream(f));
-			List<PDSVO> list = (List<PDSVO>)oin.readObject();
-			oin.close();
-			//System.out.println("Fio.list=" + list.size());
-			return list;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-    	return null;
+    	    }
+	    try {
+		    ObjectInputStream oin = new ObjectInputStream(new FileInputStream(f));
+		    List<PDSVO> list = (List<PDSVO>)oin.readObject();
+		    oin.close();
+		    // System.out.println("Fio.list=" + list.size());
+		    return list;
+	    } catch (Exception e) {
+		    e.printStackTrace();
+	    }
+	    return null;
     }
     
     public boolean serialize(List<PDSVO> list) {
-    	
-    	File f = new File(serialPath);
-
-    	try {
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
-			oos.writeObject(list);
-			oos.flush();
-			oos.close();
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-    	return false;
+	    File f = new File(serialPath);
+	    try {
+		    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
+		    oos.writeObject(list);
+		    oos.flush();
+		    oos.close();
+		    return true;
+	    } catch (Exception e) {
+		    e.printStackTrace();
+	    }
+	    return false;
     }
 
 	public PDSVO find(PDSVO key) {
