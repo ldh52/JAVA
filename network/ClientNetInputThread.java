e8d43f51d5a8acc6b4a0eff8b5c8377a9a7981f3
ldh52 <166485055+ldh52@users.noreply.github.com>
2024-08-03 12:23:01 +0900

    Update ClientNetInputThread.java

--- network/ClientNetInputThread.java
+++ network/ClientNetInputThread.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class ClientNetInputThread extends Thread 
 {
 	private ObjectInputStream oin;
-
 	static ChatMsg chatMsg;
 	
 	public ClientNetInputThread(ObjectInputStream oin) {
@@ -14,18 +13,18 @@ public class ClientNetInputThread extends Thread
 	}
 
 	@Override
-	public void run() {
+	public void run() 
+	{
 		try {
 			while(true) { 
 				ChatMsg cm = (ChatMsg)oin.readObject();
 				System.out.println("[" + cm.uid + "] " + cm.msg);
-				
 				if(cm.fdata!=null && cm.fdata.length>0) {
 					chatMsg = cm;
 					System.out.println("첨부파일("+cm.fname+")다운로드(y/n):");
 				}
 			}
-		}catch(Exception e) {
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		System.err.println("ClientNetInputThread dead");
