59ae27099594aa541c6318808b3fed8430f0a326
ldh52 <166485055+ldh52@users.noreply.github.com>
2024-08-03 13:06:37 +0900

    Update Counter.java

--- thread/Counter.java
+++ thread/Counter.java
@@ -1,7 +1,6 @@
 package thread;
 
 public class Counter {
-	
 	int cnt = 5;
 	/* cnt의 값이 항상 5~10을 유지하도록 쓰레드를 제어
 	 * cnt가 5가 되면 소비를 몀추고 생산이 되어야 한다
@@ -10,8 +9,8 @@ public class Counter {
 	 * cnt가 10에 도달하면 생산 쓰리드는 기다려야 한다(wait())
 	 * cnt가 5가 되면 생산자에게 통지를 전해야 한다(notify(), notifyAll())
 	 */
-	public synchronized void increase() {	
-
+	public synchronized void increase() 
+	{	
 		if(cnt>=10) {
 			try {
 				this.notify();
@@ -24,15 +23,14 @@ public class Counter {
 		System.out.println("올리기전: " + cnt);
 		cnt++;
 		System.out.println("올린 후: " + cnt);
-		
-		}
+	}
 	
-	public synchronized void decrease() {
+	public synchronized void decrease() 
+	{
 		if(cnt<=5) {
 			try {
-					this.notify();
-					this.wait();
-				
+				this.notify();
+				this.wait();
 			} catch (InterruptedException ie) {
 				// TODO Auto-generated catch block
 				ie.printStackTrace();
@@ -41,6 +39,5 @@ public class Counter {
 		System.out.println("내리기전: " + cnt);
 		cnt--;
 		System.out.println("내린 후: " + cnt);
-			
-		}
+	}
 }
