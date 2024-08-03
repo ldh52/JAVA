576ce78926a9756a53455beca1e713dbd7ba4fc0
ldh52 <166485055+ldh52@users.noreply.github.com>
2024-08-03 13:08:33 +0900

    Update NumThread.java

--- thread/NumThread.java
+++ thread/NumThread.java
@@ -14,7 +14,6 @@ public class NumThread extends Thread
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
