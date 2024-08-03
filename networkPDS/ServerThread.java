36330f6a2276b0cf3d1c4ec65650af2df86b08f6
ldh52 <166485055+ldh52@users.noreply.github.com>
2024-08-03 12:41:08 +0900

    Update ServerThread.java

--- networkPDS/ServerThread.java
+++ networkPDS/ServerThread.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 public class ServerThread extends Thread 
 {
-
     private Socket clientSocket;
     private ObjectInputStream oin;
     private ObjectOutputStream oos;
@@ -31,22 +30,20 @@ public class ServerThread extends Thread
     public void run() 
     {
         try {
-            while (true) 
-            {
+            while (true) {
             	PDSRequest request = (PDSRequest) oin.readObject();
-            	switch(request.getCmd()) 
-            	{
+            	switch(request.getCmd()) {
             	case UPLOAD: 
             		PDSResponse res = new PDSResponse(upload(request) ? "업로드 성공":"업로드 실패");
             		oos.writeObject(res);
             		oos.flush();
             		break;
-            	case LIST: //직렬화 파일 로드, 리스트 추출, res 오브젝트에 설정, res오브젝트 전송
+            	case LIST:	 // 직렬화 파일 로드, 리스트 추출, res 오브젝트에 설정, res오브젝트 전송
             		res = new PDSResponse(new FileIO().deserialize());
             		oos.writeObject(res);
             		oos.flush();
             		break;
-            	case DETAIL:  // 파일번호/파일이름으로 검색
+            	case DETAIL:  	// 파일번호/파일이름으로 검색
             		oos.writeObject(new PDSResponse(find(request.getPds())));
             		oos.flush();
             		break;
@@ -64,35 +61,34 @@ public class ServerThread extends Thread
             }
         } catch (Exception e) {
         	System.err.println("클라이언트가 접속 해제");
-        	//e.printStackTrace();
+        	// e.printStackTrace();
         }
         System.err.println("클라이언트를 위한 서버측 쓰레드 종료");
     }
     
-    private PDSVO find(PDSVO key)
-    {
+    private PDSVO find(PDSVO key) {
     	PDSVO found = new FileIO().find(key);
     	return found;
 	}
     
     private boolean upload(PDSRequest req)
     {
-    	PDSVO pds = req.getPds();
-		FileIO fio = new FileIO();
-		boolean saved = fio.save(pds.getFname(), pds.getFdata());
-		if(!saved) return false;
-		
-		pds.setFsize(pds.getFdata().length);
-		List<PDSVO> list = fio.deserialize();
-		if(list.size()==0) {
-			pds.setNo(1);
-		}else {
-    		//Collections.sort(list);
-    		PDSVO last = list.get(list.size()-1);
-    		pds.setNo(last.getNo()+1);
-    		pds.setFdata(null);
-		}
-		list.add(pds);
-		return fio.serialize(list);
+	    PDSVO pds = req.getPds();
+	    FileIO fio = new FileIO();
+	    boolean saved = fio.save(pds.getFname(), pds.getFdata());
+	    if(!saved) return false;
+
+	    pds.setFsize(pds.getFdata().length);
+	    List<PDSVO> list = fio.deserialize();
+	    if(list.size()==0) {
+		    pds.setNo(1);
+	    } else {
+		    // Collections.sort(list);
+		    PDSVO last = list.get(list.size()-1);
+		    pds.setNo(last.getNo()+1);
+		    pds.setFdata(null);
+	    }
+	    list.add(pds);
+	    return fio.serialize(list);
     }
 }
