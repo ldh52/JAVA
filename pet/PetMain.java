2c41ce2d711780ce4e5d3ec8a3547e421b13a565
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-08-05 21:27:40 +0900

    Update PetMain.java

--- pet/PetMain.java
+++ pet/PetMain.java
@@ -5,22 +5,21 @@ public class PetMain
 	public static void main(String[] args) 
 	{
 		/* Pet Store 관리 프로그램
-		 * Pet(no, species, weight, price)
-		 * pet.data
-		 * 이용자 입출력, FileIO, PetVO, PetMain
+		 * Pet(no, species(품종), weight, price)
+		 * pets.data
+		 * 이용자 입출력, 파일 입출력, PetVO, PetMain
 		 * 추가(a), 목록(s), 검색(f), 수정(u), 삭제(d), 종료(x) 
 		 */
-		
 		boolean go = true;
 		while(go) {
-			String m = UserIO.showmenu(); 
-			switch(m) {
-			case "x" : go = false;		break;
-			case "a" : UserIO.add();	break;
-			case "s" : UserIO.list();	break;
-			case "f" : UserIO.find();	break;
-			case "u" : UserIO.update();	break;
-			// case "d" : UserIO.delete();	break;
+			String m = UserIO.ShowMenu();
+			switch (m) {
+			case "x": go = false; break; 
+			case "a": UserIO.add(); break; 
+			case "s": UserIO.list(); break; 
+			case "f": UserIO.find(); break; 
+			case "u": UserIO.update(); break; 			
+			case "d": UserIO.delete(); break; 			
 			}
 		}
 		System.out.println("프로그램 종료");
