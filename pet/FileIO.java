efb5ddebd20231650ffdb6908acb4a1c731b0bc5
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-08-05 21:28:46 +0900

    Update FileIO.java

--- pet/FileIO.java
+++ pet/FileIO.java
@@ -8,43 +8,24 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileIO 
+public class FileIO
 {
 	static String serPath = "C:/test/data/pet.ser";
-
+	
 	public static boolean addObject(PetVO pet) {
 		List<PetVO> list = getList();
 		list.add(pet);
 		return overwrite(list);
 	}
 	
-	public static List<PetVO> getList()
-	{
-		try {
-			File ser = new File(serPath);
-			if(!ser.exists())		// 직렬화 파일이 없는 경우
-			{
-				List<PetVO> list = new ArrayList<>();
-				overwrite(list);
-			}
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(serPath));
-			List<PetVO> list = (List<PetVO>)ois.readObject();
-			return list;
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-		return null;
-	}
-	
-	private static boolean overwrite(List<PetVO> list)
+private static boolean overwrite(List<PetVO> list) 
 	{
 		try {
 			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(serPath));
@@ -52,42 +33,66 @@ public class FileIO
 			oos.close();
 			return true;
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return false;
 	}
+	
+	public static List<PetVO> getList() 
+	{
+		try {
+			File ser = new File(serPath);
+			if(!ser.exists()) { //직렬화 파일이 없는 경우
+				List<PetVO> list = new ArrayList<>();
+				overwrite(list);
+			}
+			
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(serPath));
+			List<PetVO> list = (List<PetVO>)ois.readObject();
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-	public static PetVO findByNo(int no) {
+	public static PetVO getfindByNo(int no) 
+	{
 		List<PetVO> list = getList();
 		PetVO key = new PetVO(no);
 		if(list.contains(key)) {
-			PetVO pet = list.get(list.indexOf(key));
-			return pet;
+			PetVO p = list.get(list.indexOf(key));
+			return p;
 		}
 		return null;
 	}
 
-	public static PetVO findBySpecies(String species) {
-		List<PetVO> list = getList();
-		for(int i=0;i<list.size();i++) {
-			PetVO b = list.get(i);
-			if(list.get(i).getSpecies().contains(species)) {
-				return b;
-			}
-		}
-		return null;
+	public static PetVO getfindByTitle(int no) {
+		return getfindByNo(no);
 	}
-
-	public static boolean update(PetVO pet) {
+	
+public static boolean getupdate(PetVO p) 
+	{
 		List<PetVO> list = getList();
-		if(list.contains(pet)) {
-			PetVO found = list.get(list.indexOf(pet));
-			found.setSpecies(pet.getSpecies());
-			found.setWeight(pet.getWeight());
-			found.setPrice(pet.getPrice());
-
+		if(list.contains(p)) {
+			PetVO found = list.get(list.indexOf(p));
+			found.setWeight(p.getWeight());
+			found.setPrice(p.getPrice());
 			return overwrite(list);
 		}
 		return false;
-	}	
+	}
+
+public static boolean getdelete(int no) 
+	{
+	List<PetVO> list = getList();
+	PetVO key = new PetVO(no);
+	if(list.contains(key)) {
+		list.remove(key);
+		return overwrite(list);
+	}
+	return false;
+	}
 }
